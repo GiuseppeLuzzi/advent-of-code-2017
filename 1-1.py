2752676dd3cc6b6d7e8aa9d9416ae6d1b17e0054
Giuseppe Luzzi <peppe.luzzi@gmail.com>
2017-12-01 10:40:31 +0100

    Indentation, spaces, etc

--- 1-1.py
+++ 1-1.py
@@ -1,15 +1,15 @@
 #
-#			 		Advent of Code 2017
-#					Day: 01 - Puzzle: 1
-#					  Giuseppe Luzzi
-#	https://github.com/GiuseppeLuzzi/advent-of-code-2017
+#     		 		Advent of Code 2017
+# 					Day: 01 - Puzzle: 1
+# 					  Giuseppe Luzzi
+# 	https://github.com/GiuseppeLuzzi/advent-of-code-2017
 #
 
 with open('inputs/1.txt', 'r') as input_file:
-    content = input_file.read()
+	content = input_file.read()
 
-result		= 0
-content 	= str(content)
+result = 0
+content = str(content)
 
 for i, digit in enumerate(content):
 	index = i + 1
--- 1-2.py
+++ 1-2.py
@@ -1,16 +1,16 @@
 #
-#			 		Advent of Code 2017
-#					Day: 01 - Puzzle: 1
-#					  Giuseppe Luzzi
-#	https://github.com/GiuseppeLuzzi/advent-of-code-2017
+#     		 		Advent of Code 2017
+# 					Day: 01 - Puzzle: 1
+# 					  Giuseppe Luzzi
+# 	https://github.com/GiuseppeLuzzi/advent-of-code-2017
 #
 
 with open('inputs/1.txt', 'r') as input_file:
-    content = input_file.read()
+	content = input_file.read()
 
-result		= 0
-content 	= str(content)
-half		= int(len(content) / 2)
+result = 0
+content = str(content)
+half = int(len(content) / 2)
 
 for i, digit in enumerate(content):
 	index = i + half
