6d9ff6c52d5032b67d5ea2e62d82514f76b3a349
Giuseppe Luzzi <peppe.luzzi@gmail.com>
2017-12-01 10:02:07 +0100

    Day 01 - Puzzle 2

---
+++ 1-2.py
@@ -0,0 +1,24 @@
+#
+#			 		Advent of Code 2017
+#					Day: 01 - Puzzle: 1
+#					  Giuseppe Luzzi
+#	https://github.com/GiuseppeLuzzi/advent-of-code-2017
+#
+
+with open('inputs/1.txt', 'r') as input_file:
+    content = input_file.read()
+
+result		= 0
+content 	= str(content)
+half		= int(len(content) / 2)
+
+for i, digit in enumerate(content):
+	index = i + half
+
+	if (index >= len(content)):
+		index = index - len(content)
+
+	if (digit == content[index]):
+		result = result + int(digit)
+
+print("Result: ", result)
