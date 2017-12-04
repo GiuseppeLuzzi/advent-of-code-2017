e2a3e64cd431cd523ac28a74f56c22df4e6c7e0d
Giuseppe Luzzi <peppe.luzzi@gmail.com>
2017-12-04 14:16:58 +0100

    Day 4 - Puzzle 1

---
+++ 4-1.py
@@ -0,0 +1,18 @@
+#
+#     		 		Advent of Code 2017
+# 					Day: 04 - Puzzle: 1
+# 					  Giuseppe Luzzi
+# 	https://github.com/GiuseppeLuzzi/advent-of-code-2017
+#
+
+
+with open('inputs/4.txt', 'r') as input_file:
+	rows = input_file.readlines()
+
+counter = 0;
+for row in rows:
+	row = row.rstrip('\n')
+	if len(set(row.split(' '))) != len(row.split(' ')):
+		counter += 1
+print("Valid passphares: ", len(rows) - counter)
+	
