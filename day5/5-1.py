ac1c8c1fd2db4dc96750d861c52376ad7e6551a6
Giuseppe Luzzi <peppe.luzzi@gmail.com>
2017-12-05 11:49:23 +0100

    Day 5

---
+++ day5/5-1.py
@@ -0,0 +1,22 @@
+#
+#     		 		Advent of Code 2017
+# 					Day: 05 - Puzzle: 1
+# 					  Giuseppe Luzzi
+# 	https://github.com/GiuseppeLuzzi/advent-of-code-2017
+#
+
+
+with open('input.txt', 'r') as input_file:
+	rows = input_file.read()
+
+rows = rows.split('\n')
+index = 0
+steps = 0
+
+while (index >= 0) and (index < len(rows)):
+	old_index = index
+	index = index + int(rows[index])
+	rows[old_index] = int(rows[old_index]) + 1
+	steps += 1
+
+print("Steps: %d" % (steps))
